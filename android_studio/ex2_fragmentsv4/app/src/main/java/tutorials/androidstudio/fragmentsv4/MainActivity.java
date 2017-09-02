ea320a9d589bba51cc24d80a4dffb885c80e6634
codeanticode <andres.colubri@gmail.com>
2017-09-01 21:24:01 -0300

    udpates to android studio example

--- android_studio/ex2_fragmentsv4/app/src/main/java/tutorials/androidstudio/fragmentsv4/MainActivity.java
+++ android_studio/ex2_fragmentsv4/app/src/main/java/tutorials/androidstudio/fragmentsv4/MainActivity.java
@@ -1,13 +1,13 @@
 package tutorials.androidstudio.fragmentsv4;
 
-
 import android.os.Bundle;
+import android.content.Intent;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.support.v7.app.AppCompatActivity;
 
 import processing.android.PFragment;
-import processing.android.ViewIdGenerator;
+import processing.android.CompatUtils;
 import processing.core.PApplet;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     FrameLayout frame = new FrameLayout(this);
-    frame.setId(ViewIdGenerator.getUniqueId());
+    frame.setId(CompatUtils.getUniqueViewId());
     setContentView(frame, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                                                      ViewGroup.LayoutParams.MATCH_PARENT));
 
@@ -31,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
     if (sketch != null) {
       sketch.onRequestPermissionsResult(
       requestCode, permissions, grantResults);
+    }
+  }
+
+  @Override
+  public void onNewIntent(Intent intent) {
+    if (sketch != null) {
+      sketch.onNewIntent(intent);
     }
   }
 }
